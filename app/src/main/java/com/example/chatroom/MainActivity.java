5ae43412f2c8af7cc262e4556b51462939a5c845
Cr1stal111 <nikitapolyashov3@gmail.com>
2021-11-05 05:22:56 +0300

    initial commit

---
+++ app/src/main/java/com/example/chatroom/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.chatroom;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MAX_LENGTH_MESSAGE = 99;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("messages");
+
+    EditText mEditTextMessage;
+    Button mSendButton;
+    RecyclerView mMessageRecycler;
+
+    ArrayList<String> messages = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditTextMessage = findViewById(R.id.message_input);
+        mSendButton = findViewById(R.id.send_message_b);
+        mMessageRecycler = findViewById(R.id.message_recycler);
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        DataAdapter dataAdapter = new DataAdapter(this, messages);
+
+        mMessageRecycler.setAdapter(dataAdapter);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg = mEditTextMessage.getText().toString();
+                if (msg.equals("")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enter a message!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                myRef.push().setValue(msg);
+                mEditTextMessage.setText("");
+            }
+        });
+
+        mEditTextMessage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String msg = mEditTextMessage.getText().toString();
+                if (msg.length() >= MAX_LENGTH_MESSAGE) {
+                    Toast.makeText(getApplicationContext(),
+                            "Message is too long!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String msg = snapshot.getValue(String.class);
+                messages.add(msg);
+                dataAdapter.notifyDataSetChanged();
+                mMessageRecycler.smoothScrollToPosition(messages.size() - 1);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/chatroom/ViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.chatroom;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    TextView message;
+
+    public ViewHolder(View itemView) {
+        super(itemView);
+        message = itemView.findViewById(R.id.message_text);
+    }
+}
